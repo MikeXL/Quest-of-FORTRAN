c1987199249acea3dbbc71d2141bc07ef4a18079
ML <mjliu@telus.com>
2020-07-13 13:39:29 -0700

    Update mc_pi_r.f

--- mc_pi_r.f
+++ mc_pi_r.f
@@ -1,7 +1,7 @@
 C monte carlo estimate Pi
 C always double precision for R 
 C compile using R CMD SHLIB mc_pi_r.f
-C
+C23456789
       subroutine mc_pi_r(pi)
         double precision pi
         k = 0
@@ -9,6 +9,6 @@ C
           x = rand()
           y = rand()
           if(x**2 + y**2 .lt. 1.0) k=k+1
-10      continue
+ 10     continue
         pi = real(k) / 1000000 * 4
       end subroutine mc_pi_r
